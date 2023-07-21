d8fbb9d7d8ce07d3556139e6e9d68717a3bd06f1
Rene Schwartz <rene.j.schwartz@gmail.com>
2023-07-22 02:10:27 +0300

    starting anim
    + movement is framerate independent

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -8,21 +8,39 @@ public class PlayerController : MonoBehaviour
     Transform t;
     public float movementSpeed = 0.1f;
     public FixedJoystick ilotikku;
+    public Cinemachine.CinemachineVirtualCamera vCam;
+    public GameObject beam;
 
     void Start()
     {
         t = transform;
         ilotikku = GameObject.Find("Fixed Joystick").GetComponent<FixedJoystick>();
-
-
+        vCam.enabled = false;
+        beam.SetActive(false);
+        Entrance();
     }
 
+    [ContextMenu("Entrance")]
+    void Entrance()
+    {
+        vCam.enabled = false;
+        t.DOMove(new Vector3(t.position.x + 4, t.position.y + 7, t.position.z), 3f)
+            .From()
+            .SetEase(Ease.OutSine)
+            .OnComplete(StartBeamAndCamera);
+    }
+
+    void StartBeamAndCamera()
+    {
+        beam.SetActive(true);
+        vCam.enabled = true;
+    }
     
     void Update()
     {
         if (Input.GetAxis("Horizontal") != 0)
-            t.position = t.position + Vector3.right * movementSpeed * Input.GetAxis("Horizontal");
+            t.position = t.position + Vector3.right * Time.deltaTime * movementSpeed * Input.GetAxis("Horizontal");
         else
-            t.position = t.position + Vector3.right * movementSpeed * ilotikku.Horizontal;
+            t.position = t.position + Vector3.right * Time.deltaTime * movementSpeed * ilotikku.Horizontal;
     }
 }
